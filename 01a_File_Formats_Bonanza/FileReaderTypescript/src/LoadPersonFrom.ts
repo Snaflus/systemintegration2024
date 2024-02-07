684a09217817ac9efbbb76b23c64f45db636ba76
Snaflus <cani0006@stud.kea.dk>
2024-02-07 21:13:42 +0100

    Co-authored-by: Lucas Frendorf <LucasFrendorf@users.noreply.github.com>

--- 01a_File_Formats_Bonanza/FileReaderTypescript/src/LoadPersonFrom.ts
+++ 01a_File_Formats_Bonanza/FileReaderTypescript/src/LoadPersonFrom.ts
@@ -1,24 +1,34 @@
 import { readFileSync } from 'fs';
 import { Person } from './Person';
 import YAML from 'yaml'
+import * as CSV from 'csv-string';
+var parseString = require('xml2js').parseString;
 
 export class LoadPersonFrom {
-    // static Xml(fileLocation: string): Person {
+    // module in use runs an async pipe which creates seemingly unsolvable problem
+    // static Xml(fileLocation: string): any {
     //     let jsonData;
     //     var data = readFileSync(fileLocation, 'utf8');
-    //     parseString(data, function(err, result){
-    //         jsonData = result;
+    //     parseString(data, function (err: any, result: any) {
+    //         console.log(result.me);
+    //         return result.me as Person;
     //     })
-    //     console.log(jsonData);
-    //     jsonData = JSON.stringify(jsonData)        
-    //     console.log(jsonData);
-    //     return jsonData as Person
     // }
 
-    // static Csv(fileLocation: string): Person {
-    //     var data = readFileSync(fileLocation, 'utf8');        
-    //     return CSV.parse(data) as Person
-    // }
+    static Csv(fileLocation: string): Person {
+        var data = readFileSync(fileLocation, 'utf8');
+
+        let row = CSV.parse(data)[1]
+
+        //static solution written by hand
+        let rData: Person = {
+            name: row[0],
+            age: Number(row[1]),
+            hobbies: row[2].split(",")
+        }
+
+        return rData as Person;
+    }
 
     static Yaml(fileLocation: string): Person {
         var data = readFileSync(fileLocation, 'utf8');
--- 01a_File_Formats_Bonanza/FileReaderTypescript/src/Program.ts
+++ 01a_File_Formats_Bonanza/FileReaderTypescript/src/Program.ts
@@ -7,8 +7,9 @@ console.log("JSON::: "+PersonToString(personJson));
 let personYaml = LoadPersonFrom.Yaml("..\\..\\02_Data\\me.yaml");
 console.log("YAML::: "+PersonToString(personYaml));
 
-// let personCsv = LoadPersonFrom.Csv("..\\..\\02_Data\\me.csv");
-// console.log("CSV::: "+PersonToString(personCsv));
+
+let personCsv = LoadPersonFrom.Csv("..\\..\\02_Data\\me.csv");
+console.log("CSV::: "+PersonToString(personCsv));
 
 // let personXml = LoadPersonFrom.Xml("..\\..\\02_Data\\me.xml");
 // console.log("XML::: "+PersonToString(personXml));

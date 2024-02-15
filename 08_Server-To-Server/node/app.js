3a51b61ee59ad93f6182362cd403066a3f80332b
Snaflus <7541911+Snaflus@users.noreply.github.com>
2024-02-15 10:48:07 +0100

    server to server assignment

--- 08_Server-To-Server/node/app.js
+++ 08_Server-To-Server/node/app.js
@@ -1,12 +1,18 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 
 const port = 8080;
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
+  console.log(`Server is running on port ${port}`);
 });
 
-app.get('/requestFastAPI', (req, res) => {
-    console.log('Request to FastAPI'); res.send('Request to FastAPI')
+app.get("/requestFastAPI", async (req, res) => {
+  const response = await fetch("http://localhost:8000/fastapiData");
+  const result = await response.json();
+  res.send({ data: result });
+});
+
+app.get("/expressData", async (req, res) => {
+  res.send({ data: "isRunning" });
 });
--- 08_Server-To-Server/python/main.py
+++ 08_Server-To-Server/python/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+import requests
 
 app = FastAPI()
 
 @app.get("/fastapiData")
 def serve_data():
-    return { "message": [1, 2, 3, 4, 5] }
+    return { "data": [1, 2, 3, 4, 5] }
+
+@app.get("/requestExpress")
+def get_data():
+    response = requests.get("http://localhost:8080/expressData")
+    return { "data": response.json() }

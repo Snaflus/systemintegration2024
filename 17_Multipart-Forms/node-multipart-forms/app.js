99ace9108e43e91cfe4379ec0e825fdd5eedee25
Snaflus <7541911+Snaflus@users.noreply.github.com>
2024-04-11 10:42:57 +0200

    Multiforms

--- 17_Multipart-Forms/node-multipart-forms/app.js
+++ 17_Multipart-Forms/node-multipart-forms/app.js
@@ -4,7 +4,37 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 
 import multer from "multer";
-const upload = multer({ dest: "./uploads" });
+//const upload = multer({ dest: "./uploads" });
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    console.log(file);
+    console.log(req);
+    cb(null, "./uploads");
+  },
+  filename: (req, file, cb) => {
+    const uniquePrefix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, uniquePrefix + "__" + file.originalname);
+  },
+});
+
+function fileFilter(req, file, cb) {
+  const allowedTypes = ["image/png", "image/svg"];
+
+  if (!allowedTypes.includes(file.mimetype)) {
+    cb(new Error("Only PNG and SVG images are allowed"), false);
+  } else {
+    cb(null, true);
+  }
+}
+
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 10 * 1024 * 1024, // 10 MB
+  },
+  fileFilter,
+});
 
 app.post("/form", (req, res) => {
   console.log(req.body);
@@ -18,7 +48,7 @@ app.post("/fileform", upload.single("file"), (req, res) => {
   res.send({});
 });
 
-const PORT = process.env.PORT ?? 8090;
+const PORT = process.env.PORT ?? 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
--- 17_Multipart-Forms/python-multipart-forms/main.py
+++ 17_Multipart-Forms/python-multipart-forms/main.py
@@ -1,7 +1,42 @@
-from fastapi import FastAPI, Form
+from fastapi import FastAPI, Form, File, UploadFile
+import aiofiles
+from typing import Optional
 
 app = FastAPI()
 
 @app.post("/form")
 def basic_form(username: str = Form(...), password: str = Form(default=... , min_length=8)):
+    print(username, password)
     return {"username": username, "password": password}
+
+# @app.post("/fileform")
+# def file_form(file: bytes = File(...)):
+#     with open('file', 'wb') as f:
+#         f.write(file)
+    
+#     return {"message": "File uploaded successfully"}
+
+# @app.post("/fileform")
+# async def file_form(file: UploadFile = File(...)):
+#     contents = await file.read()
+#     print(contents)
+    
+#     return {"filename": file.filename}
+
+# @app.post("/fileform")
+# async def file_form(file: UploadFile = File(...)):
+#     safe_filename = file.filename.replace("/","_").replace("\\","_")
+    
+#     with open(safe_filename.filename, 'wb') as f:
+#         while content := await file.read(1024):
+#             f.write(content)
+
+@app.post("/fileform")
+async def file_form(file: UploadFile = File(...), description: Optional[str] = Form(None)):
+    safe_filename = file.filename.replace("/","_").replace("\\","_")
+    
+    #aiofiles ensures that it's async
+    async with aiofiles.open(safe_filename.filename, 'wb') as f:
+        #walrus operator := is used to assign and return the value
+        while content := await file.read(1024): #read in 1024 chunks
+            f.write(content)

a36ee794e4f8119f5eec12eedc50ff64ec703962
Snaflus <7541911+Snaflus@users.noreply.github.com>
2024-05-05 11:00:48 +0200

    websockets update

--- 14_Websockets/node-websockets/client.js
+++ 14_Websockets/node-websockets/client.js
@@ -1,12 +1,14 @@
 import { WebSocket } from "ws";
 
 const ws = new WebSocket("ws://localhost:8080");
+console.log("Connecting to server");
 
 ws.on("open", () => {
   console.log("Connected to server");
   ws.send("Hello from Node.js client");
 
   ws.on("message", (message) => {
-    console.log("Received message:", message.toString());
+    console.log("Received message: '", message.toString(), "'");
+    ws.close();
   });
 });
--- 14_Websockets/node-websockets/server.js
+++ 14_Websockets/node-websockets/server.js
@@ -2,12 +2,13 @@ import { WebSocketServer } from "ws";
 
 const PORT = process.env.PORT ?? 8080;
 const wss = new WebSocketServer({ port: PORT });
+console.log("Server started on port", PORT);
 
 wss.on("connection", (ws) => {
   console.log("Clients connected", wss.clients.size);
 
   ws.on("message", (message) => {
-    console.log("Received message:", message.toString());
+    console.log("Received message: '", message.toString(), "'");
 
     wss.clients.forEach((client) => {
       client.send(String(message));
